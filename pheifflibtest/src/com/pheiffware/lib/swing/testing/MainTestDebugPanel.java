cefdb7ae5434f491dfd6ab236b4ed7900c0e5b46
Steve <Steve@Steve-PC.fios-router.home>
2015-09-23 19:45:00 -0400

    Line segment rename

--- pheifflibtest/src/com/pheiffware/lib/swing/testing/MainTestDebugPanel.java
+++ pheifflibtest/src/com/pheiffware/lib/swing/testing/MainTestDebugPanel.java
@@ -2,7 +2,7 @@ package com.pheiffware.lib.swing.testing;
 
 import java.awt.Color;
 
-import com.pheiffware.lib.geometry.shapes.LineSegment;
+import com.pheiffware.lib.geometry.shapes.BaseLineSegment;
 import com.pheiffware.lib.geometry.shapes.Rect;
 import com.pheiffware.lib.swing.MainFrame;
 import com.pheiffware.lib.swing.renderPanel.graphicDebug2D.GraphicDebugPanel2D;
@@ -21,11 +21,11 @@ public class MainTestDebugPanel
 			panel.addRenderable(new Rect(0.5, 0.5, 0.5, 0.5), new Color(255, 255, 0));
 			Thread.sleep(400);
 			panel.addRenderable(new Rect(-0.5, -1, 1.0, 0.5), new Color(0, 0, 255));
-			panel.addRenderable(new LineSegment(-0.5, 0.5, 0.5, -0.5), new Color(0, 255, 0));
+			panel.addRenderable(new BaseLineSegment(-0.5, 0.5, 0.5, -0.5), new Color(0, 255, 0));
 
 			Thread.sleep(400);
 			panel.clearRenderables();
-			panel.addRenderable(new LineSegment(-0.5, -0.5, 0.5, 0.5), new Color(0, 255, 0));
+			panel.addRenderable(new BaseLineSegment(-0.5, -0.5, 0.5, 0.5), new Color(0, 255, 0));
 			panel.addRenderable(new Rect(-0.5, 0, 0.5, 0.5), new Color(255, 0, 0));
 			Thread.sleep(400);
 			panel.addRenderable(new Rect(0.5, 0.5, 0.5, 0.5), new Color(255, 255, 0));
